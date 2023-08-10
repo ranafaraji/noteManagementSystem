2a759b0870781d45f61ee6c2ba42332d89d1ad4c
yceng <82166352+yceng@users.noreply.github.com>
2023-08-10 17:11:20 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './login.css';
 import './note.css';
+import './home.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
