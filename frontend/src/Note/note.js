94d1fe65dd13d44ccab4108e30309c7fadf28847
yceng <82166352+yceng@users.noreply.github.com>
2023-08-25 12:38:06 +0800

    Update note.js

--- frontend/src/Note/note.js
+++ frontend/src/Note/note.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan,faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
 
 
-const Note = ({id, text, date, handleDeleteNote}) =>{
+const Note = ({id, text, date, handleDeleteNote,handleFetchNote}) =>{
 
-    /*const [currentDateTime, setCurrentDateTime] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+    //const [editedText, setEditedText] = useState(text);
 
     useEffect(() => {
-          setCurrentDateTime(new Date());
-        }, []);*/
+        handleFetchNote();
+    }, [handleFetchNote]);
 
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+      };
+    
+      
     return(
+        
         <div className='note'>
             <span>
                 {text}
             </span>
-            <div className='notedatedel'>
+            <div className='notedatedel' >
                 <h6>{date}</h6>
-                <FontAwesomeIcon icon={faTrashCan} onClick={()=>handleDeleteNote(id)} />
-
+                <FontAwesomeIcon icon={faTrashCan} className='icon' onClick={()=>handleDeleteNote(id)} />
+                
+                <Link className='icon' to={`/editNote/${id}/${encodeURIComponent(text)}`} >
+                <FontAwesomeIcon icon={faPenToSquare} />
+                </Link>
+                
             </div>
-        </div>
+        </div> 
     )
 }
 
