2f1416268d22aa9e7ee6154e1fef36c8992d4cf3
yceng <82166352+yceng@users.noreply.github.com>
2023-08-25 12:38:55 +0800

    Update noteBook.js

--- frontend/src/Note/noteBook.js
+++ frontend/src/Note/noteBook.js
@@ -1,63 +1,86 @@
-
-import Note from './note'
-import CreateNote from './createNote';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import axios from '../api/axios';
+import React from 'react';
 import { useRef, useState, useEffect } from 'react';
-import useAuth from '../hooks/useAuth';
+import Note from './note';
+import { Link } from 'react-router-dom';
+import axios from '../api/axios';
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const NoteBook = ({notes, handleAddNote, handleDeleteNote}) =>{
+const NoteBook = ({notes, handleAddNote, handleDeleteNote,handleFetchNote }) =>{
 
+
+    
     const [userData, setUserData] = useState({ username: '' });
 
      useEffect(() => {
         fetchUserData();
+        handleFetchNote();
     }, []);
 
+
+    
      async function fetchUserData(){
         try {
-                const fetchedUserData = response.data;
-                const response =  axios.get('/get_username', {
+                const response =  await axios.get('/get_username', {
                 headers: { 'Content-Type': 'application/json' },
                 withCredentials: true
             });
+            const fetchedUserData = response.data;
             setUserData(fetchedUserData);
+            console.log(response);
+
 
             } catch (error) {
              console.error('Error fetching user data:', error);
              }
         }
 
+        const htmlToPlainText = (html) => {
+            const temporaryElement = document.createElement('div');
+            temporaryElement.innerHTML = html;
+            return temporaryElement.textContent || temporaryElement.innerText || '';
+          };
+        
+
+   
     return(
         <div id='notehome'>
-            <section id='notesec'>
-                <header className='noteheader'>
-                    <h1>My Notes</h1>
+            <div className='noteContainer'>
+                <section id='notesec'>
+                    <header className='noteheader'>
+                         <h1>My Notes</h1>
+                     </header>
 
-                    <input className='searchbar' type='text' autoFocus placeholder='Enter a keyword'></input>
-                    <button id='search'><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
-                </header>
+                    <div className='noteList'>
+                    <div className="row"> Logged in : <strong className="gfg">  {userData.username} </strong>| <a href='/home'> Logout</a></div>
 
-                <div className='noteList'>
-                <div className="row"> Logged in : <strong className="gfg">  {userData.username} </strong>| <a href='/home'> Logout</a>
-                    </div>
                     {notes.map((notes)=>(
-                        <Note
-                        id={notes.id}
-                        text={notes.text}
-                        date={notes.date}
-                        handleDeleteNote={handleDeleteNote}/>
+                        <Note 
+                        id={notes.id} 
+                        text={htmlToPlainText(notes.text)}
+                        date={notes.date} 
+                        handleDeleteNote={handleDeleteNote}
+                        handleFetchNote={handleFetchNote}/>
                     ))}
-                    <CreateNote handleAddNote={handleAddNote}
-                    />
                     
-
-                </div>
-
-            </section>
+                   <Link
+                    to={{
+                    pathname: '/createNote',
+                    state: { handleAddNote }
+                    }}>
+                    <FontAwesomeIcon className='addbtn' icon={faCirclePlus} />
+                    </Link>
+                    
+               
+              
+                    
+                    </div>
+                
+                </section>
+                
+             </div>
         </div>
+        
     )
 }
 
