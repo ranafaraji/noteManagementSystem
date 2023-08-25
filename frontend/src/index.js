754842a308f5712633722d0103953e8df5ea5bbb
yceng <82166352+yceng@users.noreply.github.com>
2023-08-25 12:35:12 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './login.css';
 import './note.css';
 import './home.css';
+import './textEditor.css'
 import App from './App';
+
 import { AuthProvider } from './context/AuthProvider';
+import { UserContextProvider } from './hooks/userContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //<QueryClientProvider client={queryClient}>
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+        <UserContextProvider>
+          <Routes>
+            
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </UserContextProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>
+  //</QueryClientProvider>
 );
